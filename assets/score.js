c6d176fc4f472b194a6027199e65a05e81404601
Kristen <killingsworth_kristen@yahoo.com>
2022-10-05 13:32:20 -0400

    updating repository so local matches cloud

--- assets/score.js
+++ assets/score.js
@@ -5,13 +5,10 @@ var scoreboardContainer = document.querySelector(`#scoreboard`);
 // retrieve initials and score from local storage
 function retrieveLocalStorage () {
     user = JSON.parse(localStorage.getItem('user'));
-    console.log(user)
-    // console.log(user)
-    // return user;
 }
 
 retrieveLocalStorage()
-for (let i=0;i<user.length;i++) {
+for (let i=0; i<user.length; i++) {
     var scoreboardEl = document.createElement("h2");
     scoreboardEl.textContent = `${user[i].initials}: ${user[i].score}`;
     scoreboardContainer.appendChild(scoreboardEl);
